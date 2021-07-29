1d0265bac57ee83f886cebafa14bee587243fd4f
wantprocess <87535590+wantprocess@users.noreply.github.com>
2021-07-29 16:11:56 +0800

    Add files via upload

---
+++ project-c54/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { View, Button, Alert } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <View
+          style={{ width: 200, height: 100, marginTop: 80, marginLeft: 80 }}>
+          <Button
+            title="Charles Babbage"
+            color="red"
+            onPress={() => Alert.alert('The father of computing.')}
+          />
+        </View>
+
+        <View
+          style={{ width: 200, height: 100, marginTop: 20, marginLeft: 80 }}>
+          <Button
+            title="Mahatma Gandhi"
+            color="blue"
+            onPress={() => Alert.alert('The father of nation.')}
+          />
+        </View>
+
+        <View
+          style={{ width: 200, height: 100, marginTop: 20, marginLeft: 80 }}>
+          <Button
+            title="Nelson Mandela"
+            color="purple"
+            onPress={() =>
+              Alert.alert('The first black president of South Africa. ')
+            }
+          />
+        </View>
+        <View
+          style={{ width: 200, height: 100, marginTop: 20, marginLeft: 80 }}>
+          <Button
+            title="Mother Teresa"
+            color="green"
+            //add code here to show alert box for Mother Teresa button
+            onPress={() => Alert.alert('Saint Teresa of Calcutta.')}
+          />
+        </View>
+      </View>
+    );
+  }
+}
